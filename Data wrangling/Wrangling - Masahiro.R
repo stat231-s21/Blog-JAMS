40836d7d379d2f3a073d6a28c648752a32130b0b
Masahiro Nishikawa <mnishikawa23@amherst.edu>
2021-04-30 12:55:29 -0400

    wrangling initiated

---
+++ Data wrangling/Wrangling - Masahiro.R
@@ -0,0 +1,17 @@
+library(rtweet)
+library(tidyverse)
+library(tidytext)
+library(textdata)
+
+stop_words <- tidytext::stop_words
+
+rt <- search_tweets(
+  q = '"water access" OR "Water access" OR "Water Access" OR "clean water"',
+  n = 5000,
+  include_rts = FALSE
+)
+
+tweets_final <- rt %>%
+  select(text) %>%
+  unnest_tokens(output = word, input = text, token = "words") %>%
+  anti_join(stop_words, by = "word")
