bbd322f51b3fed5f31e7d68e4acf8b04aaa74142
Masahiro Nishikawa <mnishikawa23@amherst.edu>
2021-05-07 14:00:12 -0400

    code recorded

--- Data wrangling/Wrangling - Masahiro.R
+++ Data wrangling/Wrangling - Masahiro.R
@@ -5,6 +5,8 @@ library(textdata)
 
 stop_words <- tidytext::stop_words
 
+
+## scraping conducted on May 4th first
 rt <- search_tweets(
   q = '"water access" OR "Water access" OR "Water Access" OR "access to clean water"
   OR "access to drinking water"',
@@ -12,13 +14,36 @@ rt <- search_tweets(
   include_rts = FALSE
 ) %>%
   select(text)
+# the above select command was run once when the csv file was created
+# and the scraping was done again excludinng the select function
 
 ##write_csv(rt, 
 ##          "/Users/masahiro/STAT231/git/Blog-JAMS/Data wrangling/tweetsMay4.csv")
 ### The above part is run on May 4th, and do not run the chunk again!
 
-tweets_final <- rt %>%
-  select(text) %>%
-  unnest_tokens(output = word, input = text, token = "words") %>%
-  anti_join(stop_words, by = "word")
+
+## from here, a scraping conducted on May 7th is enumerated
+rt2 <- search_tweets(
+  q = '"water access" OR "Water access" OR "Water Access" OR "access to clean water"
+  OR "access to drinking water"',
+  n = 5000,
+  include_rts = FALSE
+) %>%
+  filter(created_at>max(rt$created_at))
+
+rt3 <- rt2 %>% select(text)
+
+write_csv(rt3, 
+          "/Users/masahiro/STAT231/git/Blog-JAMS/Data wrangling/tweetsMay7.csv")
+
+# merge the dataset together
+may4 <- read_csv("tweetsMay4.csv")
+may7 <- read_csv("tweetsMay7.csv")
+
+# combine them together and write out
+tweets_for_blog_JAMS <- may4 %>%
+  bind_rows(may7)
+# write the dataset out
+write_csv(tweets_for_blog_JAMS, 
+          "/Users/masahiro/STAT231/git/Blog-JAMS/Data wrangling/tweetsfinal.csv")
 
