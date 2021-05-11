61f7aea5b0faf99a732dc3431fb7399a41f06434
Jamie Dailey <jcdailey21@amherst.edu>
2021-05-11 08:48:15 -0400

    Pushing shiny app code (Jamie)

--- Data wrangling/Wrangling - Jamie.R
+++ Data wrangling/Wrangling - Jamie.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(dplyr)
 
 # Import the dataset for water access in schools in 2019
-school_water_data_by_country <- read_csv("data/basic water access in schools by country.csv") %>%
+school_water_data_by_country <- read_csv("Data Wrangling/basic water access in schools by country.csv") %>%
   filter(ServiceLevel == "Basic service") %>%
   select(Country, SchoolType, Coverage) %>%
   rename(country = `Country`, schoolType = `SchoolType`, coverage = `Coverage`) 
@@ -10,14 +10,14 @@ school_water_data_by_country <- read_csv("data/basic water access in schools by 
   #rename(basicWaterCoveragePrimary = `primary`, basicWaterCoverageSecondary = `secondary`) 
 
 # Import the dataset for primary school enrollment rates
-primary_enrollment_rates <- read_csv("data/gross-enrollment-ratio-in-primary-education.csv") %>%
+primary_enrollment_rates <- read_csv("Data Wrangling/gross-enrollment-ratio-in-primary-education.csv") %>%
   group_by(Entity) %>%
   slice(which.max(Year)) %>%
   select(Entity, GrossEnrolmentRatio) %>%
   rename(primary = `GrossEnrolmentRatio`)
 
 # Import the dataset for secondary school enrollment rates
-secondary_enrollment_rates <- read_csv("data/gross-enrollment-ratio-in-secondary-education.csv") %>%
+secondary_enrollment_rates <- read_csv("Data Wrangling/gross-enrollment-ratio-in-secondary-education.csv") %>%
   group_by(Entity) %>%
   slice(which.max(Year)) %>%
   select(Entity, GrossEnrolmentRatio) %>%
@@ -39,4 +39,11 @@ final_scatter_data <- subset(scatterplot_data, (schoolType.x == "primary" & scho
                       rename(schoolType = `schoolType.x`)
 
 # Writing csv file
-write_csv(final_scatter_data, "data/school-enrollment-and-water-access.csv")
+write_csv(final_scatter_data, "Data Wrangling/school-enrollment-and-water-access.csv")
+
+# Import the dataset for water access in schools by region
+regional_water_data <- read_csv("Data Wrangling/Water in schools by region.csv") %>%
+    rename(schoolType = `Residence / School Type`, region = `Region`, coverage = `Coverage`, serviceLevel = `Service level`, year = `Year`)
+
+# Writing csv file
+write_csv(regional_water_data, "Data Wrangling/school-water-access-regional.csv")
